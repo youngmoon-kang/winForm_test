a3403d4d1955c1376551b3fd44dc2bc409ad88b1
unknown <kangoym@naver.com>
2020-08-27 22:22:34 +0900

    commit1

---
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(21, 67);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(757, 342);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(653, 25);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(691, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(21, 415);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(757, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        string src;
+        private BackgroundWorker worker;
+
+        public Form1()
+        {
+            InitializeComponent();
+            worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+
+            worker.DoWork += new DoWorkEventHandler(worker_DoWorker);
+            worker.ProgressChanged += new ProgressChangedEventHandler(worker_ProgressChanged);
+            worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(worker_RunWorkerCompleted);
+
+            worker.RunWorkerAsync();
+
+            src = "C:\\iot";
+        }
+
+        private void worker_DoWorker(object sender, DoWorkEventArgs e)
+        {
+            DirectoryInfo di = new DirectoryInfo(src);
+            FileInfo[] files = di.GetFiles();
+            int totalFiles = files.Length;
+
+            listView1.Invoke((MethodInvoker)delegate {
+                listView1.BeginUpdate();
+                listView1.View = View.Details;
+                listView1.Items.Clear();
+            });
+            int i = 0;
+            int pct = 0;
+            foreach (var fi in files) {
+                ListViewItem lvi = new ListViewItem(fi.Name);
+                lvi.SubItems.Add(fi.Length.ToString());
+                lvi.SubItems.Add(fi.LastWriteTime.ToString());
+
+                listView1.Invoke((MethodInvoker) delegate
+                {
+                    listView1.Items.Add(lvi);
+                });
+                
+                pct = ((++i * 100) / totalFiles);
+                worker.ReportProgress(pct);
+            }
+            
+        }
+
+        private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null) {
+                MessageBox.Show(e.Error.Message,"Error");
+                return;
+            }
+
+            listView1.Columns.Add("파일명", 200, HorizontalAlignment.Left);
+            listView1.Columns.Add("사이즈", 70, HorizontalAlignment.Left);
+            listView1.Columns.Add("날짜", 150, HorizontalAlignment.Left);
+
+            listView1.Invoke((MethodInvoker)delegate {
+                listView1.EndUpdate();
+            });
+            MessageBox.Show("end!");
+        }
+
+        private void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            this.progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            src = textBox1.Text;
+            worker.RunWorkerAsync();
+        }
+    }
+}
